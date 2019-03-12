104df38c204a2a26ad3ff9803fa824f694e8de61
Quoc Viet N. Tran <quocviettran95@gmail.com>
2019-03-12 20:26:42 +0100

    Fixed pics

--- app.js
+++ app.js
@@ -13,19 +13,17 @@ var chow = new pet("Chow", 8, "Best Doggo");
 var linlin = new pet("LinLin",88,"Panda");
 var data = [chow,linlin];
 
-
+app.use(express.static("pictures"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.get("/info", (req, res) => {
-    app.use(express.static('pictures'));
     res.sendFile(path.join(__dirname +"/petinfo.html"));
 });
 
 app.get("/pictures",(req,res)=>{
-    app.use(express.static('pictures'));
     res.sendFile(path.join(__dirname+"/petpictures.html"));
 })
 app.get("/data", (req, res) => {
